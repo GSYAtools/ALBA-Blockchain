c05ed602eb06b34ac57e30f024123b9b7bab3018
eromeroc97 <45209976+eromeroc97@users.noreply.github.com>
2025-12-17 11:41:27 +0100

    Traslado de la API a otro repositorio

--- fabric-api/routes/guardar.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { guardarJson } = require('../controllers/guardarController');
-
-router.post('/', guardarJson);
-
-module.exports = router;
--- fabric-api/routes/leer.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { leerJson } = require('../controllers/leerController');
-
-router.get('/:tipo/:txid', leerJson);
-
-module.exports = router;
